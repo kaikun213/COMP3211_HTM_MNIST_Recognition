fc495a4590e768550d3c240d72d0ae0f44a605e6
Jim Bridgewater <jwbwater@gmail.com>
2014-08-04 15:54:28 -0700

    Removed parameter search code and moved it to parameter_search.py.

--- train_and_test.py
+++ train_and_test.py
@@ -16,10 +16,10 @@ maxTrainingCycles - maximum number of training cycles to perform
 
 '''
 
-trainingDataset = 'DataSets/OCR/characters/hex.xml'
+trainingDataset = 'DataSets/OCR/characters/all.xml'
+testingDataset = 'DataSets/OCR/characters/all.xml'
 minAccuracy = 200.0
-maxTrainingCycles = 5
-testingDataset = 'DataSets/OCR/characters/hex.xml'
+maxTrainingCycles = 7
 
 import dataset_readers as data
 import image_encoders as encoder
@@ -34,102 +34,58 @@ from classifiers import KNNClassifier
 trainingImages, trainingTags = data.getImagesAndTags(trainingDataset)
 trainingVectors = encoder.imagesToVectors(trainingImages)
 
+# Instantiate our spatial pooler
+sp = SpatialPooler(
+  inputDimensions= (32, 32), # Size of image patch
+  columnDimensions = (32, 32),
+  potentialRadius = 10000, # Ensures 100% potential pool
+  potentialPct = 0.8,
+  globalInhibition = True,
+  localAreaDensity = -1, # Using numActiveColumnsPerInhArea
+  #localAreaDensity = 0.02, # one percent of columns active at a time
+  #numActiveColumnsPerInhArea = -1, # Using percentage instead
+  numActiveColumnsPerInhArea = 64,
+  # All input activity can contribute to feature output
+  stimulusThreshold = 0,
+  synPermInactiveDec = 0.001,
+  synPermActiveInc = 0.001,
+  synPermConnected = 0.3,
+  maxBoost = 1.0,
+  seed = 1956, # The seed that Grok uses
+  spVerbosity = 1)
 
-# Specify parameter values to search
-parameters = Parameters()
-#parameters.define("dataSet",['32.xml'])
-parameters.define("dataSet",[
-  '1.xml','2.xml', '3.xml', '4.xml', '5.xml', '6.xml', '7.xml', '8.xml',
-  '9.xml', '10.xml', '11.xml', '12.xml', '13.xml', '14.xml', '15.xml',
-  '16.xml', '17.xml', '18.xml', '19.xml', '20.xml', '21.xml', '22.xml',
-  '23.xml', '24.xml', '25.xml', '26.xml', '27.xml', '28.xml', '29.xml',
-  '30.xml', '31.xml', '32.xml', '33.xml', '34.xml', '35.xml', '36.xml',
-  '37.xml', '38.xml', '39.xml', '40.xml', '41.xml', '42.xml', '43.xml',
-  '44.xml', '45.xml', '46.xml', '47.xml', '48.xml', '49.xml', '50.xml',
-  '51.xml', '52.xml', '53.xml', '54.xml', '55.xml', '56.xml', '57.xml',
-  '58.xml', '59.xml', '60.xml', '61.xml', '62.xml'])
-#parameters.define("numCols", [(32, 32)])
-#parameters.define("numCols", [256,512,1024,2048])
-#parameters.define("synPermConn", [0.3])
-#parameters.define("synPermConn", [0.9, 0.7, 0.5, 0.3, 0.1])
-#parameters.define("synPermDecFrac", [1.0])
-#parameters.define("synPermDecFrac", [1.0, 0.5, 0.1])
-#parameters.define("synPermIncFrac", [1.0])
-#parameters.define("synPermIncFrac", [1.0, 0.5, 0.1])
+# Instantiate the spatial pooler test bench.
+tb = VisionTestBench(sp)
 
+# Instantiate the classifier
+#clf = exactMatch()
+clf = KNNClassifier()
 
-# Run the model until all combinations have been tried
-while parameters.getNumResults() < parameters.numCombinations:
+# Train the spatial pooler on trainingVectors.
+numCycles = tb.train(trainingVectors, trainingTags, clf, maxTrainingCycles,
+  minAccuracy)
 
-  # Pick a combination of parameter values
-  parameters.nextCombination()
-  dataSet = parameters.getValue("dataSet")
-  trainingDataset = 'DataSets/OCR/characters/' + dataSet
-  trainingImages, trainingTags = data.getImagesAndTags(trainingDataset)
-  trainingVectors = encoder.imagesToVectors(trainingImages)
-  testingDataset = 'DataSets/OCR/characters/' + dataSet
-  #parameters.nextRandomCombination()
-  #numCols = parameters.getValue("numCols")
-  #synPermConn = parameters.getValue("synPermConn")
-  #synPermDec = synPermConn*parameters.getValue("synPermDecFrac")
-  #synPermInc = synPermConn*parameters.getValue("synPermIncFrac")
+# Save the permanences and connections after training.
+#tb.savePermanences('perms.jpg')
+#tb.showPermanences()
+#tb.showConnections()
 
-  # Instantiate our spatial pooler
-  sp = SpatialPooler(
-    inputDimensions= (32, 32), # Size of image patch
-    columnDimensions = (32, 32),
-    potentialRadius = 10000, # Ensures 100% potential pool
-    potentialPct = 0.8,
-    globalInhibition = True,
-    localAreaDensity = -1, # Using numActiveColumnsPerInhArea
-    #localAreaDensity = 0.02, # one percent of columns active at a time
-    #numActiveColumnsPerInhArea = -1, # Using percentage instead
-    numActiveColumnsPerInhArea = 64,
-    # All input activity can contribute to feature output
-    stimulusThreshold = 0,
-    synPermInactiveDec = 0.001,
-    synPermActiveInc = 0.001,
-    synPermConnected = 0.3,
-    maxBoost = 1.0,
-    seed = 1956, # The seed that Grok uses
-    spVerbosity = 1)
+# Get testing images and convert them to vectors.
+testingImages, testingTags = data.getImagesAndTags(testingDataset)
+testingVectors = encoder.imagesToVectors(testingImages)
 
-  # Instantiate the spatial pooler test bench.
-  tb = VisionTestBench(sp)
+# Reverse the order of the vectors and tags for testing
+#print testingTags
+#testingTags = [testingTag for testingTag in reversed(testingTags)]
+#print testingTags
+#print testingVectors[0].sum()
+#testingVectors = [testingVector for testingVector in reversed(testingVectors)]
+#print testingVectors[-1].sum()
 
-  # Instantiate the classifier
-  #clf = exactMatch()
-  clf = KNNClassifier()
+# Test the spatial pooler on testingVectors.
+accuracy = tb.test(testingVectors, testingTags, clf)
 
-  # Train the spatial pooler on trainingVectors.
-  numCycles = tb.train(trainingVectors, trainingTags, clf, maxTrainingCycles,
-    minAccuracy)
-
-  # Save the permanences and connections after training.
-  #tb.savePermanences('perms.jpg')
-  #tb.showPermanences()
-  #tb.showConnections()
-
-  # Get testing images and convert them to vectors.
-  testingImages, testingTags = data.getImagesAndTags(testingDataset)
-  testingVectors = encoder.imagesToVectors(testingImages)
-
-  # Reverse the order of the vectors and tags for testing
-  #print testingTags
-  #testingTags = [testingTag for testingTag in reversed(testingTags)]
-  #print testingTags
-  #print testingVectors[0].sum()
-  #testingVectors = [testingVector for testingVector in reversed(testingVectors)]
-  #print testingVectors[-1].sum()
-
-  # Test the spatial pooler on testingVectors.
-  accuracy = tb.test(testingVectors, testingTags, clf)
-
-  # Add results to the list
-  parameters.appendResults([accuracy, numCycles])
-
-
-parameters.printResults(["Percent Accuracy", "Training Cycles"], [", %.2f", ", %d"])
-print "The maximum number of training cycles is set to:", maxTrainingCycles
+print "Number of training cycles:", numCycles
+print "Accuracy:", accuracy
 
 
